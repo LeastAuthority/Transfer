b118f9c149dd682707a3815cd94815ce453f9da4
Bryan White <bryanchriswhite@gmail.com>
2021-07-01 20:14:17 +0200

    wip terraform

--- terraform/static.tf
+++ terraform/static.tf
@@ -29,28 +29,7 @@ resource "aws_iam_user" "bryan_white" {
 
 resource "aws_iam_user_login_profile" "bryan_white" {
   user = aws_iam_user.bryan_white.name
-  pgp_key = "mQENBFP6FQEBCADVEjNNt2Sk3fvI51M3g5Lq/wsF5TDKsWDnbqIBJlPnpI3Nt7FosdC0bfcxzPne"+
-  "b6NpyV1vZj2xvO+cwJn/sXtZA9H+YpD1wd37TxWeLfQvHUCErafN23zFeelnWrbaFLzgAv6tDgoI"+
-  "shJ5mBb/SDxam8vZ34T4FwN9wWkKe7NtOWK2VmmZWJRbmi+8bVyjRr4tcl5b4/Yx285GpTOvYt0K"+
-  "dPQoHeD7756UxGPtl7IfcZbbVB/g79FS+d2vldEGql3bhthRpBv90Jrr3IsLSXBSp1lPyOGmFEmM"+
-  "LY34eenDKkGXpCKnyV2NA+jj97UOf49lXJAk3TAmbqhyLThIgMh9ABEBAAG0KUJyeWFuIEMgV2hp"+
-  "dGUgPGJyeWFuY2hyaXN3aGl0ZUBnbWFpbC5jb20+iQFUBBMBCgA+AhsDBQsJCAcDBRUKCQgLBRYC"+
-  "AwEAAh4BAheAFiEEFTHxe7DY+b4CVZtFhc+ybjWHL5kFAmAulcsFCQ/258oACgkQhc+ybjWHL5lA"+
-  "Swf7BU78djn550HTFahwbNhdMLQ078CUmEq2fT7BYsk/DUQ6rFfEKljazh/ltus18rdZ4dklTRyp"+
-  "XZICCQLPPR9T1zQizXHENj0Nf9Osqs3ehqmuIp+2INIxV9/Fa5uVbl56tsPtQyGyjx4hGdkznTyh"+
-  "h2EjG9ITs+Ee1rE2S482hlQ80Zl03VlUZ5A09y+BYwo4JPBf+82eKBvlQOJ1a8gbEJ8AB47s7bTL"+
-  "iZkimp4TN9vTSdmaSuDKG1YYr1oJprSKf+m9NCK39OnrHTTw3mWQodVIHWZMb5+YVdmCyg3ol1Ut"+
-  "yvQ8Mt8ugxQo+Ww6Nb36ZBzOfznoFIPXCFMzYzxk6bkBDQRT+hUBAQgAzq91M3ONROsS/4++QGQE"+
-  "WijvGGuwHpw7/4ZvTPjsIELrlczb6nenqDQOWcIMS4BN/Ika6lSJvFBCmYs6tDfV4ufrepjlOP97"+
-  "14PfAJf/guuf2+Fg5tMh663k6cRF2XCAy714bbfKTxBGReVYDAzARywMLXaNiEATWhJOpyVYX20d"+
-  "cXoAsiOq+YphUbgmT3Yxg4qGHlGSIknuYcMTgI76yAtAe1rzGZVeSipoi7OZNldTzQxAeEAT0Jlr"+
-  "et8g9SO/PgQQOTEt9Ch0Q2sAEAqAfB5VMrAOkwZa/1tKUvloiZEPBiPA5lI28+0styDCXAqIIBdO"+
-  "Vlp13wNJK1+m/n0YLwARAQABiQE8BBgBCgAmAhsMFiEEFTHxe7DY+b4CVZtFhc+ybjWHL5kFAmAu"+
-  "lecFCQ/25+YACgkQhc+ybjWHL5mGqwf/dMBv5KKL2tlajin48KYCJBcdX9u/SqPwQEkQKWBknlAe"+
-  "y2a+Fi0dZm44WATknaDNMECbPIJGS5IE4OISn8vdKQg0r1MgfJJljTHhS+rpA7dlHG/PAojYVSeu"+
-  "pPXr7QdARE2hVo3AKDq5S7VE8RuE46skHYFwUtjX5AC4jFkTIPl7s51Kt3DVVRytHJDppTlYXWRA"+
-  "I5r2yCd/LXvWfMIXPaAhDvED0vrz5MN7k9ReT0dnu0hPX6Uyzi32zEduQl6bxCsLS/JkWs38cRcs"+
-  "drmA4dDua/jk9MyL/bH61iylf2neD8F1hSUfRx1QxnDFwSVS3gbdCqRAHxIheIYLAqbmxA=="
+  pgp_key = "mQENBFP6FQEBCADVEjNNt2Sk3fvI51M3g5Lq/wsF5TDKsWDnbqIBJlPnpI3Nt7FosdC0bfcxzPneb6NpyV1vZj2xvO+cwJn/sXtZA9H+YpD1wd37TxWeLfQvHUCErafN23zFeelnWrbaFLzgAv6tDgoIshJ5mBb/SDxam8vZ34T4FwN9wWkKe7NtOWK2VmmZWJRbmi+8bVyjRr4tcl5b4/Yx285GpTOvYt0KdPQoHeD7756UxGPtl7IfcZbbVB/g79FS+d2vldEGql3bhthRpBv90Jrr3IsLSXBSp1lPyOGmFEmMLY34eenDKkGXpCKnyV2NA+jj97UOf49lXJAk3TAmbqhyLThIgMh9ABEBAAG0KUJyeWFuIEMgV2hpdGUgPGJyeWFuY2hyaXN3aGl0ZUBnbWFpbC5jb20+iQFUBBMBCgA+AhsDBQsJCAcDBRUKCQgLBRYCAwEAAh4BAheAFiEEFTHxe7DY+b4CVZtFhc+ybjWHL5kFAmAulcsFCQ/258oACgkQhc+ybjWHL5lASwf7BU78djn550HTFahwbNhdMLQ078CUmEq2fT7BYsk/DUQ6rFfEKljazh/ltus18rdZ4dklTRypXZICCQLPPR9T1zQizXHENj0Nf9Osqs3ehqmuIp+2INIxV9/Fa5uVbl56tsPtQyGyjx4hGdkznTyhh2EjG9ITs+Ee1rE2S482hlQ80Zl03VlUZ5A09y+BYwo4JPBf+82eKBvlQOJ1a8gbEJ8AB47s7bTLiZkimp4TN9vTSdmaSuDKG1YYr1oJprSKf+m9NCK39OnrHTTw3mWQodVIHWZMb5+YVdmCyg3ol1UtyvQ8Mt8ugxQo+Ww6Nb36ZBzOfznoFIPXCFMzYzxk6bkBDQRT+hUBAQgAzq91M3ONROsS/4++QGQEWijvGGuwHpw7/4ZvTPjsIELrlczb6nenqDQOWcIMS4BN/Ika6lSJvFBCmYs6tDfV4ufrepjlOP9714PfAJf/guuf2+Fg5tMh663k6cRF2XCAy714bbfKTxBGReVYDAzARywMLXaNiEATWhJOpyVYX20dcXoAsiOq+YphUbgmT3Yxg4qGHlGSIknuYcMTgI76yAtAe1rzGZVeSipoi7OZNldTzQxAeEAT0Jlret8g9SO/PgQQOTEt9Ch0Q2sAEAqAfB5VMrAOkwZa/1tKUvloiZEPBiPA5lI28+0styDCXAqIIBdOVlp13wNJK1+m/n0YLwARAQABiQE8BBgBCgAmAhsMFiEEFTHxe7DY+b4CVZtFhc+ybjWHL5kFAmAulecFCQ/25+YACgkQhc+ybjWHL5mGqwf/dMBv5KKL2tlajin48KYCJBcdX9u/SqPwQEkQKWBknlAey2a+Fi0dZm44WATknaDNMECbPIJGS5IE4OISn8vdKQg0r1MgfJJljTHhS+rpA7dlHG/PAojYVSeupPXr7QdARE2hVo3AKDq5S7VE8RuE46skHYFwUtjX5AC4jFkTIPl7s51Kt3DVVRytHJDppTlYXWRAI5r2yCd/LXvWfMIXPaAhDvED0vrz5MN7k9ReT0dnu0hPX6Uyzi32zEduQl6bxCsLS/JkWs38cRcsdrmA4dDua/jk9MyL/bH61iylf2neD8F1hSUfRx1QxnDFwSVS3gbdCqRAHxIheIYLAqbmxA=="
   password_reset_required = false
 }
 
@@ -213,7 +192,7 @@ locals {
   acm_certificate_arn = data.aws_acm_certificate.mw4all_website_test_arn.arn
 }
 
-resource "aws_cloudfront_distribution" "mw4all_website_test" {
+resource "aws_cloudfront_distribution" "winden_website" {
   origin {
     domain_name = local.s3_website_domain_name
     origin_id = local.origin_id
