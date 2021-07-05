aeb0815dd2b34bded4d8c522fb442d51272e434d
Bryan White <bryanchriswhite@gmail.com>
2021-07-05 16:00:35 +0200

    add version

--- src/components/CardModal.vue
+++ src/components/CardModal.vue
@@ -1,23 +1,39 @@
 <template>
     <ion-card>
         <slot></slot>
+        <div class="version">
+            <ion-text color="dark-grey">
+                v0.0.0-alpha.0
+            </ion-text>
+        </div>
     </ion-card>
 </template>
 
 <script>
 import {defineComponent} from "vue";
-import {IonCard} from "@ionic/vue";
+import {IonCard, IonText} from "@ionic/vue";
 
 export default defineComponent({
     name: "CardModal",
     components: {
         IonCard,
+        IonText,
     }
 })
 </script>
 
-<style scoped>
+<style lang="css" scoped>
 ion-card {
     position: relative;
+}
+
+ion-card > .version {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    margin-right: var(--ion-margin);
+    margin-bottom: var(--ion-margin);
+    padding: var(--ion-padding);
+    background: rgba(255, 255, 255, 0.6);
 }
 </style>
