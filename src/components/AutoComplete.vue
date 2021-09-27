f6fa30bc637bea7186e53078c3672998ad1a0c64
Bryan White <bryanchriswhite@gmail.com>
2021-09-27 16:48:11 +0200

    wip autocomplete

---
+++ src/components/AutoComplete.vue
@@ -0,0 +1,21 @@
+<template>
+    <ion-content class="ion-padding">
+        Popover Content
+    </ion-content>
+</template>
+
+<script>
+import {IonContent} from '@ionic/vue';
+import {defineComponent} from "vue";
+
+export default defineComponent({
+    name: "AutoComplete",
+    components: {
+        IonContent,
+    }
+});
+</script>
+
+<style scoped>
+
+</style>
