b930becd86ed9aefe862ad67725978785303c29c
Bryan White <bryanchriswhite@gmail.com>
2021-12-03 12:03:58 +0100

    Add deploy scripts (#63)

---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const {argv, env, exit} = require('process');
+const {execSync} = require('child_process');
+
+const {loadEnv} = require('./common');
+
+const stage = argv[2];
+if (!stage) {
+    console.error("Usage: deloy.js <stage name>")
+    exit(1);
+}
+
+const envPath = `.${stage}.env`;
+loadEnv(envPath);
+
+const bucket = env['S3_BUCKET'];
+const command = `aws s3 sync ./dist ${bucket}`;
+
+try {
+    execSync(command, {stdio: 'pipe'});
+} catch (error) {
+    console.error(error);
+    exit(3);
+}
