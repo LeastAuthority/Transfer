540ff8ba1119ec21d439c3519e9cdc7b7b14a93a
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2022-02-10 00:15:35 +0530

    initial version of the build script

---
+++ scripts/build.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+const {argv, env, exit} = require('process');
+const {execSync} = require('child_process');
+
+const {loadEnv} = require('./common');
+
+const stage = argv[2];
+if (!stage) {
+    console.error("Usage: build.js <stage name>")
+    exit(1);
+}
+
+const envPath = `.${stage}.build.env`;
+loadEnv(envPath);
+
+const host = env['STAGE_HOSTNAME'];
+const relayURL = env['STAGE_RELAY_URL'];
+const mailboxURL = env['STAGE_MAILBOX_URL'];
+
+console.log(`host: ${host}`)
+console.log(`relayURL: ${relayURL}`)
+console.log(`mailboxURL: ${mailboxURL}`)
+
+// "build:playground": "NODE_ENV=playground yarn build:worker && NODE_ENV=playground vue-cli-service build",
+const command = `yarn build:worker && vue-cli-service build`;
+
+try {
+    execSync(command);
+} catch (error) {
+    console.error(error);
+    exit(3);
+}
+
