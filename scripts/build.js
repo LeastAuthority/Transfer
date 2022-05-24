fe943d18901ba8fcc790764d6e96751d94efb9c5
Justus Tumacder <justusft@gmail.com>
2022-05-24 14:20:41 +0800

    Load env before command1

--- scripts/build.js
+++ scripts/build.js
@@ -11,17 +11,17 @@ if (!stage) {
   exit(1);
 }
 
+const envPath = `.${stage}.build.env`;
+if (Fs.existsSync(envPath)) {
+  loadEnv(envPath);
+}
+
 const command1 = `NODE_ENV=${stage} ./node_modules/.bin/vue-cli-service build`;
 try {
   execSync(command1);
 } catch (error) {
   console.error(error);
   exit(3);
-}
-
-const envPath = `.${stage}.build.env`;
-if (Fs.existsSync(envPath)) {
-  loadEnv(envPath);
 }
 const command2 = `NODE_ENV=${stage} yarn build:worker`;
 
